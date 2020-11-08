12eebe62ba15e5b42458f5a7db73b037427f61c9
cristian spiridon <scristian71@yahoo.com>
2020-11-08 19:09:19 +0000

    add migrations

---
+++ 02/demos/start-end/src/ImageGallery.API/Migrations/20201108111835_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImageGallery.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Images",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "Images",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Images",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Images",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Images",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "Images",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Images",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Images",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid));
+        }
+    }
+}
