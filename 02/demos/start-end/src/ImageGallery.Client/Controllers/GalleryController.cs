799e48f39d4f57dfb2b14483b9da6d7ae0fa23cc
cristian spiridon <scristian71@yahoo.com>
2020-11-14 20:54:25 +0000

    add authorize for add image at level of client

--- 02/demos/start-end/src/ImageGallery.Client/Controllers/GalleryController.cs
+++ 02/demos/start-end/src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -138,6 +138,7 @@ namespace ImageGallery.Client.Controllers
             return RedirectToAction("Index");
         }
 
+        [Authorize(Roles = "PayingUser")]
         public IActionResult AddImage()
         {
             return View();
@@ -145,6 +146,7 @@ namespace ImageGallery.Client.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "PayingUser")]
         public async Task<IActionResult> AddImage(AddImageViewModel addImageViewModel)
         {
             if (!ModelState.IsValid)
